56536dcfd6dd39d5abfcf476f8af541a866f7e74
Jack Hegarty <patrick.jack.hegarty@gmail.com>
2017-01-03 21:29:08 +0000

    Added Mailgun Settigns to ENVIORNMETN rb File

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# MailGun Settings
+
+ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'secure-sands-96002.herokuapp.com',
+    :authentication => :plain,
+}
+
+ActionMailer::Base.delivery_method = :smtp
